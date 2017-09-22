5901cac6ce8b45a4514679781cf0bc60d9df6112
jony <jonatanmarquez88@gmail.com>
2017-09-22 14:09:34 -0300

    correccion para home

--- src/javascript/home.js
+++ src/javascript/home.js
@@ -9,8 +9,22 @@ $(function(){
 		$(this).find(".parrafo").animate({"margin-top":"30%"},1500);		
 		});
 
-	$("div").mousemove(function(){		
-		$(this).find(".parrafo").show();
-		$(this).find(".parrafo").animate({"margin-top":"30%"},1500);		
-	});
+
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1500);
+        return false;
+    });
+
+
 })

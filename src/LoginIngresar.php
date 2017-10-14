54446004f3ae1ab3b4748647db7b5c42d1593eaf
Belu-87 <mbblasquiz@gmail.com>
2017-10-13 21:14:59 -0300

    Mas modificaciones en el login

--- src/LoginIngresar.php
+++ src/LoginIngresar.php
@@ -23,7 +23,7 @@
             	<div class="form-box">
                 	<div class="form-top">
                 		<div class="text-center">
-                			<h3>Ingresa a nuestro sitio</h3>
+	              			<h3>Ingresa a nuestro sitio</h3>
                 		</div>
                 		<div class="form-top-right">
                 			<i class="fa fa-key"></i>
@@ -42,6 +42,24 @@
 	                        <button type="submit" class="btn">Ingresar</button>
 	                    </form>
                     </div>
+
+                    <div class="form-group">
+						<div class="row">
+							<div class="col-lg-10">
+								<div class="text-center">
+									<a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">¿Has olvidado tu contraseña?</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- ver bien como hacer para que vay a la pagina Registrar -->
+					<div class="row"> 
+						<div class="col-md-24"><!-- ko if: svr.Am && !svr.aj --> 
+						<div class="text-13 form-group no-margin-bottom" data-bind="css: { 'no-margin-bottom': !svr.Bn &amp;&amp; !svr.showCantAccessAccountLink },htmlWithBindings: html['WF_STR_SignUpLink_Text'], childBindings: { 'signup': { href: svr.g, css: { 'display-inline-block': true }, ariaLabel: str['WF_STR_SignupLink_AriaLabel_Text'], click: signup_onClick } }">¿No tiene una cuenta? <a href="https://signup.live.com/signup?wa=wsignin1.0&amp;rpsnv=13&amp;ct=1507939016&amp;rver=6.7.6643.0&amp;wp=MBI_SSL&amp;wreply=https%3a%2f%2foutlook.live.com%2fowa%2f&amp;id=292841&amp;cbcxt=out&amp;fl=wld&amp;contextid=3310C71A5FCBE307&amp;bk=1507939018&amp;uiflavor=web&amp;uaid=a62f7b7cda344eae8ea85896156c9515&amp;mkt=ES-US&amp;lc=21514" id="signup" class="display-inline-block" aria-label="Crear una cuenta Microsoft">Cree una.</a>
+			            </div><!-- /ko --><!-- ko if: svr.Bn --><!-- /ko --><!-- ko if: svr.showCantAccessAccountLink --><!-- /ko --> 
+			            </div> 
+           			</div>
+
                 </div>
             
             	<div class="social-login text-center">
--- src/LoginRegistrar.php
+++ src/LoginRegistrar.php
@@ -18,7 +18,44 @@
 <body>
 
 	<?php include("head.php");?>
-	<form>
+
+	<div class="form-box">
+		<div class="form-top">
+    		<div class="text-center">
+    			<h3>Sign up now</h3>
+        		<p>Fill in the form below to get instant access:</p>
+    		</div>
+    		<div class="form-top-right">
+    			<i class="fa fa-pencil"></i>
+    		</div>
+        </div>
+        <div class="form-bottom">
+            <form role="form" action="" method="post" class="registration-form">
+            	<div class="form-group">
+            		<label class="sr-only" for="form-first-name">First name</label>
+                	<input type="text" name="form-first-name" placeholder="First name..." class="form-first-name form-control" id="form-first-name">
+                </div>
+                <div class="form-group">
+                	<label class="sr-only" for="form-last-name">Last name</label>
+                	<input type="text" name="form-last-name" placeholder="Last name..." class="form-last-name form-control" id="form-last-name">
+                </div>
+                <div class="form-group">
+                	<label class="sr-only" for="form-email">Email</label>
+                	<input type="text" name="form-email" placeholder="Email..." class="form-email form-control" id="form-email">
+                </div>
+                <div class="form-group">
+                	<label class="sr-only" for="form-about-yourself">About yourself</label>
+                	<textarea name="form-about-yourself" placeholder="About yourself..." class="form-about-yourself form-control" id="form-about-yourself"></textarea>
+                </div>
+                <button type="submit" class="btn">Sign me up!</button>
+            </form>
+        </div>
+	</div>
+
+
+
+
+<!-- 	<form>
 		<br><br>
 		<label> Nombre: </label>
 		<input id="Nombre"></input>
@@ -26,8 +63,8 @@
 		<label> Apellido: </label>
 		<input id="Apellido"></input>
 		<br><br>
-		<!-- fecha de nacimiento para promociones cumpleaños??
-		 -->
+		fecha de nacimiento para promociones cumpleaños??
+		
 		<label> Email: </label>
 		<input id="mail"></input>
 		<br><br>
@@ -43,7 +80,7 @@
 		  <div class="loader"></div>
 		  <div class="done">SUCCESS</div>
 		</button>
-	</form>
+	</form> -->
 
 </body>
 

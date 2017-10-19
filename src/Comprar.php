c0f5a0b96b820956eee4e9648f27dedbb7709240
jony <jonatanmarquez88@gmail.com>
2017-10-19 09:58:12 -0300

    modificaciones a pagina comprar

---
+++ src/Comprar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html lang="es">
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+	<script data-require="tether@*" data-semver="1.4.0" src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
+    <link data-require="bootstrap@*" data-semver="4.0.5" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+    <script data-require="bootstrap@*" data-semver="4.0.5" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"></script>
+
+    <link  rel="stylesheet" type="text/css" href="css/EstiloComprar.css" />
+
+	<title>Comprar</title>
+
+</head>
+
+<body>
+	<?php include("head.php");?>	
+
+	<p class="aparece"> pagina en construccion 
+	</p>
+
+	<img class="imagen aparece" src="../Imagenes/under_construction.png">
+
+</body>
+
+<footer>
+
+</footer>
+
+
+</html>

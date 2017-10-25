a9dbb26f3d4ff03661c54c5772f3fb102bb38167
jony <jonatanmarquez88@gmail.com>
2017-10-24 23:49:52 -0300

    correccion home

--- src/home.php
+++ src/home.php
@@ -18,49 +18,46 @@
 
 
 <body>
-
-	<div id="containerGeneral">
-		<?php include("head.php");?>
-		<div id="container1">
-			<div class="parrafo">
-				<h1>Calidad</h1>
-				<p>Nuestros productos son elaborados bajo una estricta
-				selección de la materia prima.</p>
-			</div>		
+	<?php include("head.php");?>
+	<div id="container1">
+		<div class="parrafo">
+			<h1>Calidad</h1>
+			<p>Nuestros productos son elaborados bajo una estricta
+			selección de la materia prima.</p>
+		</div>		
+	</div>
+	<div id="container2">
+		<div class="parrafo">
+			<h1>Precio</h1>
+			<p>Nos preocupamos para que disfrutes cosas ricas al mejor precio del mercado.</p>
 		</div>
-		<div id="container2">
-			<div class="parrafo">
-				<h1>Precio</h1>
-				<p>Nos preocupamos para que disfrutes cosas ricas al mejor precio del mercado.</p>
-			</div>
+	</div>
+	<div id="container3">
+		<div class="parrafo">
+			<h1>Servicio</h1>
+			<p>Te llevamos nuestros productos para que no tengas que pensar en otro compromiso.</p>
 		</div>
-		<div id="container3">
-			<div class="parrafo">
-				<h1>Servicio</h1>
-				<p>Te llevamos nuestros productos para que no tengas que pensar en otro compromiso.</p>
-			</div>
-		</div>
-		<div id="container4">
-			<div class="parrafo">
-				<h1>Te cuidamos</h1>
-				<p>Sabemos que tu pedido es único y por eso nos esforzamos para que te sientas contento cada vez que pensás en nosotros.</p>
-			</div>		
-		</div>
-		<div id="container5">
+	</div>
+	<div id="container4">
+		<div class="parrafo">
+			<h1>Te cuidamos</h1>
+			<p>Sabemos que tu pedido es único y por eso nos esforzamos para que te sientas contento cada vez que pensás en nosotros.</p>
+		</div>		
+	</div>
+	<div id="container5">
 
-			<div class="parrafo">
-				<h1>Contactanos</h1>
-			<form>
-				<label>Enterate nuestras novedades </label>
-				<br>
-				<label> E-mail: </label>
-				<input></input>
-				<button>Enviar</button>
-			</form>
+		<div class="parrafo">
+			<h1>Contactanos</h1>
+		<form>
+			<label>Enterate nuestras novedades </label>
+			<br>
+			<label> E-mail: </label>
+			<input></input>
+			<button>Enviar</button>
+		</form>
 
-			<a href="#" class="scrollup"><img class="scrollup" src="https://image.flaticon.com/icons/svg/25/25282.svg"></img></a>
+		<a href="#" class="scrollup"><img class="scrollup" src="https://image.flaticon.com/icons/svg/25/25282.svg"></img></a>
 
-			</div>
 		</div>
 	</div>
 

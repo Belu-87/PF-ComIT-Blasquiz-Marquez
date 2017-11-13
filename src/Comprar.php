611fda46f038823d346689f35e341bcebe4472fa
jony <jonatanmarquez88@gmail.com>
2017-11-13 16:35:57 -0300

    frontend pagina "comprar"

--- src/Comprar.php
+++ src/Comprar.php
@@ -27,27 +27,20 @@
 	<div class="container">
 		<div class="row">
 			<div class="table-responsive">
-			  <table class="table aparece">
+			  <table id="tabla" class="table aparece">
 			    
 			    <thead>
 			    	<th>Producto</th>
 			    	<th>Detalle</th>
 					<th>Cantidad</th>
-					<th>Precio Unitario</th>
+					<th>Precio<br>Unitario</th>
 					<th>Total</th>
 					<th></th>
 					<th></th>
 			    </thead>
-
-			    <tr class="aparece">
+			    <tr id="fila1">
 					<td>
-<!-- 						<select class="form-control">
-							<option value="1">Producto 1</option>
-							<option value="2">Producto 2</option>
-							<option value="3">Producto 3</option>
-							<option value="4">Producto 4</option>
-						</select> -->
-						<select class="fstElement fstSingleMode fstNoneSelected form-control" name="uno" placeholder="opcion">
+						<select id="producto1" class="fstElement fstSingleMode fstNoneSelected form-control" name="uno" placeholder="opcion">
 						    <option value="Bangladesh">Bangladesh</option>
 						    <option value="Barbados">Barbados</option>
 						    <option value="Belarus">Belarus</option>
@@ -55,7 +48,7 @@
 						</select>
 					</td>
 					<td>
-						<select class="multipleSelect form-control" multiple name="language" placeholder="opciones">
+						<select id="detalle1" class="multipleSelect form-control" multiple name="language" placeholder="opciones">
 						    <option value="Bangladesh">Bangladesh</option>
 						    <option value="Barbados">Barbados</option>
 						    <option value="Belarus">Belarus</option>
@@ -63,26 +56,26 @@
 						</select>
 					</td>					
 					<td>
-						<input class="form-control" type="number" id="new_country">
+						<input class="form-control" type="number" step="1" id="cantidad1">
 					</td>
 					<td>						
 						<div class="input-group">						
 							<span class="input-group-addon">$</span>
-							<input class="form-control" type="number" step="0.01" id="new_age">
+							<input disabled="disabled" id="precioUnit1" class="form-control" type="number" step="0.01">
 						</div>
 					</td>
 					<td>
 						<div class="input-group">						
 							<span class="input-group-addon">$</span>
-							<input class="form-control" type="number" step="0.01" id="new_age">
+							<input disabled="disabled" id="precioTotal1" class="form-control" type="number" step="0.01">
 						</div>
 					</td>	
 					<td>
-						<img class="imagen-fila aparece" src="../Imagenes/delete.png">
+						<img id="eliminar" style="display: none;" class="imagen-fila eliminar aparece" src="../Imagenes/delete.png">
 					</td>
 					<!-- <td><input type="button" class="add" onclick="add_row();" value="Add Row"></td> -->
 					<td>
-						<img class="imagen-fila aparece" src="../Imagenes/add.png">
+						<img id="agregar" class="imagen-fila agregar aparece" src="../Imagenes/add.png">
 					</td>
 				</tr>
 
--- src/javascript/comprar.js
+++ src/javascript/comprar.js
@@ -1,9 +1,59 @@
+ 
  $(function () 
  {
-	$('.multipleSelect').fastselect();
+ 	var i=2;
+	
+	$('#producto1').fastselect();
+	$('#detalle1').fastselect();
 
-	$('.fstSingleMode').fastselect();
+	
+	$('#agregar').click(function(){
+		AddFila();		
+	})
 	
 
- });
+
  
+
+
+ function AddFila(){
+		var data='<tr id="fila'+i+'" class="aparece"><td><select id="producto'+i+'" class="fstElement fstSingleMode fstNoneSelected form-control" ';
+		data+='name="uno" placeholder="opcion"><option value="Bangladesh">Bangladesh</option> ';
+		data+='<option value="Barbados">Barbados</option><option value="Belarus">Belarus</option> ';
+		data+='<option value="Belgium">Belgium</option></select></td><td>  ';
+		data+='<select class="fstElement fstSingleMode fstNoneSelected form-control" multiple name="language" placeholder="opciones" id="detalle'+i+'"> ';
+		data+='<option value="Afghanistan">Afghanistan</option> ';
+		data+='<option value="Albania">Albania</option> ';
+		data+='<option value="Algeria">Algeria</option> ';
+		data+='<option value="Andorra">Andorra</option> ';
+		data+='<option value="Angola">Angola</option> ';
+		data+='</select> ';		
+		data+='</td><td><input class="form-control" type="number" step="1" id="cantidad'+i+'"></td> ';
+		data+='<td><div class="input-group"><span class="input-group-addon">$</span> ';
+		data+='<input disabled="disabled" id="precioUnit'+i+'" class="form-control" type="number" ';
+		data+='step="0.01"></div></td><td><div class="input-group"><span class="input-group-addon">$</span> ';
+		data+='<input disabled="disabled" id="precioTotal'+i+'" class="form-control" type="number" step="0.01"></div> ';
+		data+='</td><td><img id="eliminar'+i+'" class="imagen-fila eliminar aparece" src="../Imagenes/delete.png"></td> ';
+		data+='<!-- <td><input type="button" class="add" value="Add Row"></td> --> ';
+		data+='<td><img style="display: none;" id="agregar'+i+'" class="imagen-fila aparece" src="../Imagenes/add.png"></td></tr> ';
+
+	
+
+		$('#tabla tr:last').after(data);	
+
+		var campo="#producto"+i;
+		$(campo).fastselect();
+		campo="#detalle"+i;
+		$(campo).fastselect();
+
+		campo="#eliminar"+i;
+		$(campo).on('click',function(){			
+			$(campo).fadeOut(1000,function(){$(this).closest('tr').remove();});
+		});
+
+		i+=1;
+ }
+
+
+
+ });

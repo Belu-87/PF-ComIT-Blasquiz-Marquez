f87de96fc6ff539d8e8e8810387aa332c1a3f80d
Belu-87 <mbblasquiz@gmail.com>
2017-11-20 17:38:56 -0300

    Modificacion de zoom y paginas en catalogo.

--- src/logout.php
+++ src/logout.php
@@ -4,6 +4,12 @@ session_unset();
     $_SESSION['FBID'] = NULL;
     $_SESSION['FULLNAME'] = NULL;
     $_SESSION['EMAIL'] =  NULL;
-    $_SESSION['usuario'] =  NULL;
+    $_SESSION['usuario'] =  NULL; 
+
+    //ver este o destroy
+session_cache_expire();
+    
 header("Location: home.php");        // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
+
+session_destroy();
 ?>
